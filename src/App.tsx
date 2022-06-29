a6a86eb0498f1e054988daa1c1c5e6f971d34a07
Floristq <56862489+Floristq@users.noreply.github.com>
2022-06-29 22:47:08 +1000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Giphy from "./components/Giphy";
+import "./styles.css";
+const App = () => {
+  return (
+    <div>
+      <Giphy />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <i className="fas fa-cog fa-4x fa-spin" />
+    </div>
+  );
+};
+
+export default Loader;
